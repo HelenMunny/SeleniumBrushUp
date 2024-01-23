c4d0d5ec28553b8d805b9ebc4e431928771b58ef
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2024-01-23 05:40:34 -0500

    Ebay and twitter

--- src/main/java/org/example/Ebay.java
+++ src/main/java/org/example/Ebay.java
@@ -1,15 +1,21 @@
 package org.example;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Main {
-    public static void main(String[] args) {
+public class Ebay {
+    public static void main(String[] args) throws InterruptedException{
         System.setProperty("webdriver.chrome.driver","C:\\Users\\mahmu\\IdeaProjects\\SeleniumBrushUp\\src\\main\\resources\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.setBinary("C:\\Users\\mahmu\\Downloads\\chrome-win64\\chrome-win64\\chrome.exe");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://twitter.com/");
+        driver.get("https://www.ebay.com/");
+
+        driver.findElement(By.xpath("//input[@id=\"gh-ac\"]")).sendKeys("Moisturizer");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id=\"gh-btn\"]")).click();
     }
 }
